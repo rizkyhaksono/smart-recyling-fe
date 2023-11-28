6c84d753fb134c48cb42f3c2f95ef6027f2658c1
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-11-28 23:03:43 +0700

    feat(RHFTextField): validation password and email textfield

--- src/components/hook-form/RHFTextField.jsx
+++ src/components/hook-form/RHFTextField.jsx
@@ -21,19 +21,25 @@ export default function RHFTextField({ name, label, helperText, type }) {
         control={control}
         render={({ field }) => (
           <>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-textColor">
+            <label htmlFor={name} className="block mb-2 text-sm font-medium text-textColor">
               {label}
             </label>
-            <input
-              name="password"
-              id="password"
-              className="bg-gray-50 border border-gray-300 text-textColor text-sm rounded-lg pr-10 block w-full p-2.5"
-              required
-              {...field}
-              type={type}
-              placeholder={helperText}
-              value={typeof field.value === "number" && field.value === 0 ? "" : field.value}
-            />
+            <div className="relative">
+              <input
+                id={name}
+                className="bg-gray-50 border border-gray-300 text-textColor text-sm rounded-lg pr-10 block w-full p-2.5"
+                required
+                {...field}
+                type={type === "password" && showPassword ? "text" : type}
+                placeholder={helperText}
+                value={typeof field.value === "number" && field.value === 0 ? "" : field.value}
+              />
+              {type === "password" && (
+                <div className="absolute top-2 right-2 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                </div>
+              )}
+            </div>
           </>
         )}
       />
