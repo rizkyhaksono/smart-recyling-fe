83166f4d9990d13331ce87c40e229d142344a728
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-12-14 03:12:47 +0700

    feat (exchange): modal view and popover

--- src/components/CardExchange.jsx
+++ src/components/CardExchange.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { List, Modal, Spin } from "antd";
+import { List, Modal, Spin, Button, Card, Popover } from "antd";
 import { useGetItemQuery } from "../redux/api/itemApi";
 
 export default function CardExchange() {
@@ -29,6 +29,14 @@ export default function CardExchange() {
     setSelectedItem(null);
   };
 
+  const renderPopoverContent = (item) => (
+    <div className="text-base text-textColor">
+      <p className="font-semibold mb-3">{`Name: ${item.name}`}</p>
+      <p>{`Points: ${item.points}`}</p>
+      <p>{`Total: ${item.total}`}</p>
+    </div>
+  );
+
   return (
     <>
       <List
@@ -38,27 +46,41 @@ export default function CardExchange() {
           onChange: (page) => {
             console.log(page);
           },
-          pageSize: 4,
+          pageSize: 2,
         }}
-        className="mb-5"
+        className="mb-5 container mx-auto"
         dataSource={useItem.data}
         renderItem={(item) => (
           <List.Item key={item.id} onClick={() => handleItemClick(item)}>
-            <div>
-              {/* <img src={`/exchange-${item.id}.png`} alt={item.name} /> */}
-              <h3>{item.name}</h3>
-              <p>{`Points: ${item.points}`}</p>
-              <p>{`Total: ${item.total}`}</p>
-            </div>
+            <Popover content={renderPopoverContent(item)} trigger="hover">
+              <div className="flex flex-row gap-5 items-center cursor-pointer" onClick={() => handleItemClick(item)}>
+                <img className="sm:w-40 md:w-56 lg:w-64 xl:w-1/3 max-[640px]:w-32 rounded-xl" src={`/exchange-${item.id}.png`} alt={item.name} />
+                <Card className="text-textColor font-medium">
+                  <p className="text-2xl">{item.name}</p>
+                  <p className="text-base mt-4">{`Points: ${item.points}`}</p>
+                  <p className="text-base">{`Total: ${item.total}`}</p>
+                </Card>
+              </div>
+            </Popover>
           </List.Item>
         )}
       />
 
-      <Modal title={selectedItem ? selectedItem.name : ""} visible={modalOpen} onCancel={closeModal} footer={null}>
+      <Modal
+        title={<div className="text-textColor text-2xl font-bold mb-5">{selectedItem ? selectedItem.name : ""}</div>}
+        open={modalOpen}
+        onCancel={closeModal}
+        footer={[
+          <Button key="close" onClick={closeModal}>
+            Close
+          </Button>,
+        ]}
+      >
         {selectedItem && (
           <>
-            <p>{`Points: ${selectedItem.points}`}</p>
-            <p>{`Total: ${selectedItem.total}`}</p>
+            <img src={`/exchange-${selectedItem.id}.png`} alt={selectedItem.name} />
+            <p className="text-textColor text-lg font-medium mt-5">{`Points: ${selectedItem.points}`}</p>
+            <p className="text-textColor text-lg font-medium">{`Total: ${selectedItem.total}`}</p>
           </>
         )}
       </Modal>
