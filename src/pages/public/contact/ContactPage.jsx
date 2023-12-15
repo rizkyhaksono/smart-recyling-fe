81ae72781dc4b534ef46bb9b4f08a30f8df92544
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-12-15 21:06:09 +0700

    feat (contact): implement emailjs

--- src/pages/public/contact/ContactPage.jsx
+++ src/pages/public/contact/ContactPage.jsx
@@ -1,9 +1,45 @@
 "use client";
 
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import { notification, Button } from "antd";
 import NavbarComponent from "../../../components/NavbarComponent";
 import FooterComponent from "../../../components/FooterComponent";
 
 export default function ContactPage() {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const openNotification = (type, message) => {
+    notification[type]({
+      message,
+      placement: "topRight",
+    });
+  };
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    const serviceId = "service_6pv8zsc";
+    const templateId = "template_q3jcfdh";
+    const userId = "TNBzy6ZKUvJ5YW-wN";
+
+    setLoading(true);
+
+    try {
+      const response = await emailjs.send(serviceId, templateId, { to_email: email, subject, message }, userId);
+      console.log("Email sent:", response);
+      openNotification("success", "Email sent successfully!");
+    } catch (error) {
+      console.error("Error sending email:", error);
+      openNotification("error", "Error sending email.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <NavbarComponent />
@@ -13,7 +49,7 @@ export default function ContactPage() {
       </div>
       <section className="container mx-auto px-5">
         <div className="py-8 px-4 mx-auto max-w-screen-xl">
-          <form action="#" className="space-y-8">
+          <form onSubmit={sendEmail} className="space-y-8">
             <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
                 Your email
@@ -24,6 +60,7 @@ export default function ContactPage() {
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                 placeholder="name@gmail.com"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
@@ -36,6 +73,7 @@ export default function ContactPage() {
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Let us know how we can help you"
                 required
+                onChange={(e) => setSubject(e.target.value)}
               />
             </div>
             <div className="sm:col-span-2">
@@ -47,11 +85,12 @@ export default function ContactPage() {
                 rows="6"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Leave a comment..."
+                onChange={(e) => setMessage(e.target.value)}
               ></textarea>
             </div>
-            <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white bg-primary rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">
+            <Button type="text" htmlType="submit" loading={loading} className="pb-8 pt-3 px-6 text-sm font-medium text-center text-white bg-primary rounded-lg">
               Send message
-            </button>
+            </Button>
           </form>
         </div>
       </section>
