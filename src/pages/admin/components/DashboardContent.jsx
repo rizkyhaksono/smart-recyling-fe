8d3c0cec94e8a146b4c8b1268f686dd327511bf1
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-12-17 01:49:09 +0700

    feat (admin): refactor dashboard

--- src/pages/admin/components/DashboardContent.jsx
+++ src/pages/admin/components/DashboardContent.jsx
@@ -1,4 +1,16 @@
-import { ArrowUpOutlined, UsergroupDeleteOutlined, CarryOutOutlined, FileTextOutlined, BarsOutlined } from "@ant-design/icons";
+import {
+  AppstoreAddOutlined,
+  FileDoneOutlined,
+  CalendarOutlined,
+  ContainerOutlined,
+  UsergroupDeleteOutlined,
+  CarryOutOutlined,
+  FileTextOutlined,
+  BarsOutlined,
+  PictureOutlined,
+  ScheduleOutlined,
+  CheckCircleOutlined,
+} from "@ant-design/icons";
 import { Card, Col, Row, Statistic, Spin } from "antd";
 import { useGetAllUsersQuery } from "../../../redux/api/userApi";
 import { useGetReportsQuery } from "../../../redux/api/reportApi";
@@ -9,212 +21,138 @@ export default function DashboardContent() {
   const { data: usersData, isLoading: usersLoading } = useGetAllUsersQuery();
   const { data: reportsData, isLoading: reportsLoading } = useGetReportsQuery();
   const { data: itemsData, isLoading: itemsLoading } = useGetItemQuery();
-  const { data: evenstData, isLoading: eventLoading } = useGetEventsQuery();
+  const { data: eventsData, isLoading: eventLoading } = useGetEventsQuery();
 
   if (usersLoading || reportsLoading || itemsLoading || eventLoading) {
     return <Spin size="large" className="flex justify-center items-center" />;
   }
 
-  const adminCount = usersData.data.reduce((count, user) => count + (user.role === "ADMIN" ? 1 : 0), 0);
-  const userCount = usersData.data.reduce((count, user) => count + (user.role === "USER" ? 1 : 0), 0);
-  const pengepulCount = usersData.data.reduce((count, user) => count + (user.role === "PENGEPUL" ? 1 : 0), 0);
+  const countByRole = (role) => usersData.data.reduce((count, user) => count + (user.role === role ? 1 : 0), 0);
+
+  const uniqueLocationsInMalang = reportsData.data
+    .filter((report) => report.location.toLowerCase() === "malang")
+    .map((report) => report.location)
+    .filter((value, index, self) => self.indexOf(value) === index);
+  const totalUniqueLocationsInMalang = uniqueLocationsInMalang.length;
+
+  const calculateTotalForEachItem = (itemsData) => {
+    const totals = {
+      Tshirt: 0,
+      Totebag: 0,
+      Bottle: 0,
+      Cutlery: 0,
+    };
+    itemsData.data.forEach((item) => {
+      switch (item.name.toLowerCase()) {
+        case "kaos":
+          totals.Tshirt += item.total;
+          break;
+        case "totebag":
+          totals.Totebag += item.total;
+          break;
+        case "bottle":
+          totals.Bottle += item.total;
+          break;
+        case "cutlery":
+          totals.Cutlery += item.total;
+          break;
+        default:
+          break;
+      }
+    });
+    return totals;
+  };
+  const itemsTotals = calculateTotalForEachItem(itemsData);
+
+  const calculateTotalEventsPerDay = (eventsData) => {
+    if (!eventsData?.data || !Array.isArray(eventsData.data)) {
+      return "";
+    }
+    const eventCountsPerDay = {};
+    eventsData.data.forEach((event) => {
+      if (event?.created_at) {
+        const eventDay = new Date(event.created_at).toLocaleDateString();
+        eventCountsPerDay[eventDay] = (eventCountsPerDay[eventDay] || 0) + 1;
+      }
+    });
+    const filteredEventCountsPerDay = Object.fromEntries(Object.entries(eventCountsPerDay).filter(([count]) => count > 0));
+    const resultString = Object.entries(filteredEventCountsPerDay)
+      .map(([date, count]) => `${date}: ${count}`)
+      .join(", ");
+    return resultString;
+  };
+  const totalEventsPerDay = calculateTotalEventsPerDay(eventsData);
+
+  const calculateTotalReportsPerDay = (reportsData) => {
+    if (!reportsData?.data || !Array.isArray(reportsData.data)) {
+      return "";
+    }
+    const reportCountsPerDay = {};
+    reportsData.data.forEach((report) => {
+      if (report?.created_at) {
+        const reportDay = new Date(report.created_at).toLocaleDateString();
+        reportCountsPerDay[reportDay] = (reportCountsPerDay[reportDay] || 0) + 1;
+      }
+    });
+    const filteredReportCountsPerDay = Object.fromEntries(Object.entries(reportCountsPerDay).filter(([count]) => count > 0));
+    const resultString = Object.entries(filteredReportCountsPerDay)
+      .map(([date, count]) => `${date}: ${count}`)
+      .join(", ");
+    return resultString;
+  };
+
+  const totalReportsPerDay = calculateTotalReportsPerDay(reportsData);
+
+  const renderStatisticCard = (title, value, prefix, suffix) => (
+    <Col xs={24} sm={12} md={6} lg={6}>
+      <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1 hover:bg-slate-50">
+        <Statistic title={title} value={value} prefix={prefix} suffix={suffix} />
+      </Card>
+    </Col>
+  );
 
   return (
     <>
       <p className="font-bold text-3xl text-textColor mt-3 mb-10">Dashboard Admin Smart Recycling</p>
+
       <p className="font-semibold text-xl text-textColor mb-2">Overview Users Data</p>
       <Card bordered={true}>
         <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={6} lg={6}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Records"
-                value={usersData.data.length}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<UsergroupDeleteOutlined />}
-                suffix="records"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={6} lg={6}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Users"
-                value={userCount}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<UsergroupDeleteOutlined />}
-                suffix="users"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={6} lg={6}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Pengepul"
-                value={pengepulCount}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<UsergroupDeleteOutlined />}
-                suffix="pengepul"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={6} lg={6}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Admin"
-                value={adminCount}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<UsergroupDeleteOutlined />}
-                suffix="admin"
-              />
-            </Card>
-          </Col>
+          {renderStatisticCard("Total Records", usersData.data.length, <UsergroupDeleteOutlined />, "records")}
+          {renderStatisticCard("Total Users", countByRole("USER"), <UsergroupDeleteOutlined />, "users")}
+          {renderStatisticCard("Total Pengepul", countByRole("PENGEPUL"), <UsergroupDeleteOutlined />, "pengepul")}
+          {renderStatisticCard("Total Admin", countByRole("ADMIN"), <UsergroupDeleteOutlined />, "admin")}
         </Row>
       </Card>
+
       <p className="font-semibold text-xl text-textColor my-2">Overview Reports Data</p>
       <Card bordered={true}>
         <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Reports"
-                value={reportsData.data.length}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<CarryOutOutlined />}
-                suffix="reports"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Location In Malang"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="in Malang"
-              />
-            </Card>
-          </Col>
+          {renderStatisticCard("Total Reports", reportsData.data.length, <CarryOutOutlined />, "reports")}
+          {renderStatisticCard("Total Location In Malang", totalUniqueLocationsInMalang, <PictureOutlined />, "on Malang")}
+          {renderStatisticCard("Total Location In Malang", totalReportsPerDay, <CalendarOutlined />, "per day")}
+          {renderStatisticCard("Total Location In Malang", totalUniqueLocationsInMalang, <PictureOutlined />, "on Malang")}
         </Row>
       </Card>
+
       <p className="font-semibold text-xl text-textColor my-2">Overview Events Data</p>
       <Card bordered={true}>
         <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Events"
-                value={evenstData.data.length}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<FileTextOutlined />}
-                suffix="events"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Events"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"
-              />
-            </Card>
-          </Col>
+          {renderStatisticCard("Total Events", eventsData.data.length, <FileTextOutlined />, "events")}
+          {renderStatisticCard("Total Events", totalEventsPerDay, <CalendarOutlined />, "per day")}
+          {renderStatisticCard("Total Events", totalEventsPerDay, <ScheduleOutlined />, "latest")}
+          {renderStatisticCard("Total Events", totalEventsPerDay, <ScheduleOutlined />, "latest")}
         </Row>
       </Card>
+
       <p className="font-semibold text-xl text-textColor my-2">Overview Items Data</p>
       <Card bordered={true}>
         <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Items"
-                value={itemsData.data.length}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<BarsOutlined />}
-                suffix="items"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total T-Shirt"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="items"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Totebag"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="items"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Bottle"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="items"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <Card bordered={false} className="hover:shadow-md transition-transform transform-gpu hover:translate-y-1">
-              <Statistic
-                title="Total Cutlery"
-                value={11.28}
-                precision={2}
-                valueStyle={{
-                  color: "#3f8600",
-                }}
-                prefix={<ArrowUpOutlined />}
-                suffix="items"
-              />
-            </Card>
-          </Col>
+          {renderStatisticCard("Total Items", itemsData.data.length, <BarsOutlined />, "items")}
+          {renderStatisticCard("Total T-Shirt", itemsTotals.Tshirt, <FileDoneOutlined />, "T-Shirts")}
+          {renderStatisticCard("Total Totebag", itemsTotals.Totebag, <ContainerOutlined />, "Totebags")}
+          {renderStatisticCard("Total Bottle", itemsTotals.Bottle, <AppstoreAddOutlined />, "Bottles")}
+          {renderStatisticCard("Total Cutlery", itemsTotals.Cutlery, <CheckCircleOutlined />, "Cutleries")}
         </Row>
       </Card>
     </>
